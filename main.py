b94078800d3406edb3f455a73bb4c7436d6df6a9
Mehmet Gorkem Arslan <mehmetgorkemarslan@gmail.com>
2023-12-14 16:06:31 +0300

    Now, the decks of players are managed with a class.

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from random import shuffle
 
+class Hand:
+    def __init__(self, hand):
+        self.hand = hand
+
+
 def game_start():
+    #It creates a deck, shuffles, and draws a total of 4 cards for 2 people
     deck = [
     2,3,4,5,6,7,8,9,"J","Q","K","A",
     2,3,4,5,6,7,8,9,"J","Q","K","A",
     2,3,4,5,6,7,8,9,"J","Q","K","A",
     2,3,4,5,6,7,8,9,"J","Q","K","A"]
     shuffle(deck)
-    gamer_hand = [deck.pop(i) for i in range(2)]
-    croupier_hand = [deck.pop(i) for i in range(2)]
-    return gamer_hand, croupier_hand, deck
+    gamer = Hand([deck.pop(i) for i in range(2)])
+    croupier = Hand([deck.pop(i) for i in range(2)])
+    return gamer, croupier, deck
 
 def main():
-    gamer_hand, croupier_hand, current_deck = game_start()
-    print(gamer_hand, croupier_hand, current_deck)
+    gamer, croupier, current_deck = game_start()
+    print(gamer.hand, current_deck)
     pass
 
 if __name__ == "__main__":
